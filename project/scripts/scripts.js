85205384e11052397aec80829d0e90a029a21177
MuhireB1 <muhirebienvenue@gmail.com>
2026-08-04 11:41:39 +0200

    siteplan II

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
   renderProducts();
   refreshUI();
 });
-
+ 
 // --- CONFIG & DATA ARCHITECTURE ---
 const CONFIG = {
   CART_KEY: 'eshop-cart',
